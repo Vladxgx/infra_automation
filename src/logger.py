e6da89a8f16fdfa787b9b3514defa56cb2b9524f
VladG <Vladxv2@gmail.com>
2025-12-12 00:58:25 +0200

    added proviosioning logger

--- src/logger.py
+++ src/logger.py
@@ -1,9 +1,13 @@
 import logging
 from pathlib import Path
 
+LOG_DIR = path("logs")
+LOG_DIR.mkdir(exist_ok=True)
+
 LOG_PATH = Path("logs/app.log")
 LOG_PATH.parent.mkdir(parents=True, exist_ok=True)
 
+#main logger
 logger = logging.getLogger("infra_simulator")
 logger.setLevel(logging.INFO)
 
@@ -12,3 +16,13 @@ if not logger.handlers:
     formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
+
+#provisioning bash/nginx logger
+proviosioning_logger = logging.getLogger("provisioning")
+proviosioning_logger.setLevel(logging.INFO)
+
+if not proviosioning_logger.handlers:
+    prov_handler = logging.FileHandler(PROV_LOG_PATH)
+    prov_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    prov_handler.setFormatter(prov_formatter)
+    proviosioning_logger.addHandler(prov_handler)
